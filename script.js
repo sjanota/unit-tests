e12683a0814f41ce878a123ec97c48b64ec9fe47
dud-ka <kaardud@gmail.com>
2017-08-05 12:35:11 +0200

    test_shouldReturnFalseIfLetterNotInWord

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function findLetterInWord(letter, word) {
-    return
+
+    return true;
 }
 
-function test_shouldReturnTrueIfLetterInWord () {
+function test_shouldReturnTrueIfLetterInWord() {
     var letter = "o";
     var word = "Karolina";
     var expected = true;
@@ -11,5 +12,26 @@ function test_shouldReturnTrueIfLetterInWord () {
         console.log("OK");
     } else {
         console.log("Result:", result, "Expeted:", expected);
+    }
+}
+
+function test_shouldReturnFalseIfLetterNotInWord() {
+    var letter = "b";
+    var word = "Karolina";
+    var expected = false;
+    var result = findLetterInWord(letter, word);
+    if (result === expected) {
+        console.log("OK");
+    } else {
+        console.log("Result:", result, "Expeted:", expected);
+    }
+}
+
+function runTests() {
+    for(k in window) {
+        if (k.startsWith("test_")) {
+            console.log(k);
+            (window[k])();
+        }
     }
 }
