74b8aafa2bc00fe9a83622dcd467ece3b5bef5bd
dud-ka <kaardud@gmail.com>
2017-08-05 12:13:47 +0200

    test_shouldReturnTrueIfLetterInWord

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function findLetterInWord(letter, word) {
     return
 }
+
+function test_shouldReturnTrueIfLetterInWord () {
+    var letter = "o";
+    var word = "Karolina";
+    var expected = true;
+    var result = findLetterInWord(letter, word);
+    if (result === expected) {
+        console.log("OK");
+    } else {
+        console.log("Result:", result, "Expeted:", expected);
+    }
+}
